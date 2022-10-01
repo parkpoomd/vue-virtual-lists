5f8445a412c9a1b5c7c707290199fc4f4fafccd4
parkpoomd <parkpoom.d@gmail.com>
2022-10-01 11:22:17 +0700

    completed

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue';
+import { useInfiniteScroll, useVirtualList } from '@vueuse/core';
+
+const data = ref(Array.from(Array(50).keys(), () => 'Lorem ipsum'));
+
+const { list, containerProps, wrapperProps } = useVirtualList(data, {
+  // Keep `itemHeight` in sync with the item's row.
+  itemHeight: 96,
+});
+
+useInfiniteScroll(
+  containerProps.ref,
+  () => {
+    // load more
+    data.value.push(...Array.from(Array(10).keys(), () => 'More lorem ipsum'));
+  },
+  { distance: 10 } // in pixels
+);
+</script>
+
+<template>
+  <div v-bind="containerProps" class="h-screen p-2 rounded">
+    <div v-bind="wrapperProps" class="max-w-sm mx-auto">
+      <div
+        v-for="{ index, data } in list"
+        :key="index"
+        class="rounded-lg h-[80px] flex flex-col px-4 justify-center bg-neutral-800 mb-4 border-neutral-600"
+      >
+        <h2 class="mb-2 text-2xl">Item #{{ index }}</h2>
+        <p class="text-sm">{{ data }}</p>
+      </div>
+    </div>
+  </div>
+</template>
